6f423c9628ce514dea2e957c350d7bb851f695c3
Milton Figueredo Miles <gratno@gmail.com>
2023-02-21 14:23:06 -0500

    Dividiendo strings

--- src/br/com/java/io/test/TestLectura2.java
+++ src/br/com/java/io/test/TestLectura2.java
@@ -3,6 +3,7 @@ package br.com.java.io.test;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class TestLectura2 {
@@ -19,13 +20,33 @@ public class TestLectura2 {
 		
 		String linea; 
 		
-		/*while ((linea = scanner.nextLine()) != null  && scanner.hasNextLong()) {
+		while (scanner.hasNextLine() && ( linea = scanner.nextLine()) != null ) {
+			//System.out.println(linea);
+			
+			//USANDO SPLIT
+			/*String[] lineas = linea.split(",");
+			//System.out.println(lineas);
+			System.out.println(Arrays.toString(lineas));
+		
+			System.out.println(lineas[3]);*/
+			
+			//USANDO SCANNER
+			Scanner lineaScanner = new Scanner(linea);
+		
+			lineaScanner.useDelimiter(",");
+			
+			String valor;
+			
+			while (lineaScanner.hasNextLine() && ( valor = lineaScanner.nextLine())  != null ) {
+				
+				System.err.println(valor);
+			}
+			
+		}
+		
+		/*while ((linea = scanner.nextLine()) != null ) {
 			System.out.println(linea);
 		}*/
-		
-		while ((linea = scanner.nextLine()) != null ) {
-			System.out.println(linea);
-		}
 		
 	}
 
