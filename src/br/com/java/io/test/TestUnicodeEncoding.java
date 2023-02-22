88ec3853625e450aa797d819ec17816e6a3ec826
Milton Figueredo Miles <gratno@gmail.com>
2023-02-21 19:31:51 -0500

    Charset - Codepoint - Encoder - UTF-8

--- src/br/com/java/io/test/TestUnicodeEncoding.java
+++ src/br/com/java/io/test/TestUnicodeEncoding.java
@@ -1,10 +1,12 @@
 package br.com.java.io.test;
 
+import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 public class TestUnicodeEncoding {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnsupportedEncodingException {
 		
 		
 		
@@ -12,8 +14,18 @@ public class TestUnicodeEncoding {
 		
 		System.out.println("Codepoint: " + letra.codePointAt(0));
 		
-		Charset charset =  Charset.defaultCharset();
-		System.out.println("Charset: " + Charset.defaultCharset());
+		//Charset charser =  Charset.defaultCharset();
+		Charset charset =  Charset.forName("windows-1252");
+		System.out.println("Charset: " + charset.displayName());
+		
+		//byte[] bytes = letra.getBytes();
+		//byte[] bytes = letra.getBytes("UTF-16");
+		byte[] bytes = letra.getBytes(StandardCharsets.ISO_8859_1);
+		
+		System.out.println("Cantidad de bytes: " + bytes.length);
+		
+		//System.out.println(charset.availableCharsets());
+		
 	}
 
 }
